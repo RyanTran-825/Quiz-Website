6d75b8628e251a5ba9767c9951aec72b15ea0231
RyanTran-825 <153715767+RyanTran-825@users.noreply.github.com>
2024-12-27 01:27:58 +0700

    first commit

---
+++ question.js
@@ -0,0 +1,119 @@
+let questions = [
+  {
+    numb1: 1,
+    question: "Máy tính điện tử là gì?",
+    answer: "C. Thiết bị lưu trữ và xử lý thông tin",
+    option: [
+      "A. Thiết bị lưu trữ thông tin",
+      "B. Thiết bị số hóa và biến đổi thông tin",
+      "C. Thiết bị lưu trữ và xử lý thông tin",
+      "D. Thiết bị tạo và biến đổi thông tin",
+    ],
+  },
+  {
+    numb1: 2,
+    question:
+      "Máy tính đầu tiên ENIAC sử dụng linh kiện nào trong số các linh kiện sau?",
+    answer: "C. Đèn điện tử ",
+    option: [
+      "A. Transistor lưỡng cực",
+      "B. Transistor trường",
+      "C. Đèn điện tử ",
+      "D. IC bán dẫn",
+    ],
+  },
+  {
+    numb1: 3,
+    question: "Trong các phát biểu sau, phát biểu nào đúng?",
+    answer:
+      "A. Phần cứng của máy tính bao gồm các đối tượng vật lý như: bản mạch chính, bộ nhớ RAM, ROM, đĩa cứng, màn hình ",
+    option: [
+      "A. Phần cứng của máy tính bao gồm các đối tượng vật lý như: bản mạch chính, bộ nhớ RAM, ROM, đĩa cứng, màn hình ",
+      "B. Phần cứng của máy tính bao gồm các đối tượng như: bản mạch chính, bộ nhớ RAM, bộ nhớ ROM, đĩa cứng, màn hình và chương trình được cài đặt trong ROM",
+      "C. Phần cứng của máy tính là chương trình được cài đặt trong bộ nhớ ROM",
+      "D. Phần cứng của máy tính chính là bộ xử lý trung tâm",
+    ],
+  },
+  {
+    numb1: 4,
+    question: "Các thành phần cơ bản của một máy tính gồm:",
+    answer:
+      "D. Bộ nhớ trong, CPU, bộ nhớ ngoài, bộ phối ghép vào ra và thiết bị ngoại vi",
+    option: [
+      "A. Bộ nhớ trong, CPU và khối phối ghép vào ra",
+      "B. Bộ nhớ trong, CPU và thiết bị ngoại vi",
+      "C. Bộ nhớ trong, CPU, khối phối ghép vào ra và thiết bị ngoại vi",
+      "D. Bộ nhớ trong, CPU, bộ nhớ ngoài, bộ phối ghép vào ra và thiết bị ngoại vi",
+    ],
+  },
+  {
+    numb1: 5,
+    question: "Phần dẻo (Firmware) trong máy tính là gì?",
+    answer:
+      "A. Phần mềm được đặt vào bên trong các mạch điện tử trong quá trình sản xuất ",
+    option: [
+      "A. Phần mềm được đặt vào bên trong các mạch điện tử trong quá trình sản xuất ",
+      "B. Hệ điều hành",
+      "C. Các Driver cho các thiết bị phần cứng và các mạch hỗ trợ phối ghép vào ra cho máy tính",
+      "D. Phần mềm hệ thống",
+    ],
+  },
+  {
+    numb1: 6,
+    question: "Một ví dụ về phần dẻo (Firmware) trong máy tính là:",
+    answer: "B. Chương trình điều khiển trong ROM BIOS",
+    option: [
+      "A. Hệ điều hành MS DOS",
+      "B. Chương trình điều khiển trong ROM BIOS",
+      "C. Chương trình Driver cho Card màn hình của máy tính",
+      "D. Phần mềm ứng dụng của người dùng",
+    ],
+  },
+  {
+    numb1: 7,
+    question:
+      "Việc trao đổi dữ liệu giữa thiết bị ngoại vi và máy tính được thực hiện qua:",
+    answer: "B. Một cổng",
+    option: [
+      "A. Một thanh ghi điều khiển",
+      "B. Một cổng",
+      "C. Thanh ghi AX",
+      "D. Thanh ghi cờ",
+    ],
+  },
+  {
+    numb1: 8,
+    question: "Phần mềm của máy tính là:",
+    answer: "C. Chương trình được cài đặt trong bộ nhớ ROM",
+    option: [
+      "A. Các bộ điều phối thiết bị giúp cho việc ghép nối và ra được thực hiện một cách linh hoạt.",
+      "B. Cơ cấu trao đổi dữ liệu giữa các thiết bị phần cứng trong máy tính",
+      "C. Chương trình được cài đặt trong bộ nhớ ROM",
+      "D. Bộ vi xử lý và các vi mạch hỗ trợ cho nó",
+    ],
+  },
+  {
+    numb1: 9,
+    question:
+      "Trong các bộ phận sau, bộ phận nào không thuộc bộ xử lý trung tâm:",
+    answer: "A. Đơn vị phối ghép vào ra",
+    option: [
+      "A. Đơn vị phối ghép vào ra",
+      "B. Khối số học và logic",
+      "C. Tập các thanh ghi đa năng",
+      "D. Khối điều khiển",
+    ],
+  },
+  {
+    numb1: 10,
+    question:
+      "Trong các bộ phận sau, bộ phận nào không thuộc bộ xử lý trung tâm:",
+    answer: "A. Bộ nhớ trong",
+    option: [
+      "A. Bộ nhớ trong",
+      "B. Khối số học và logic",
+      "C. Tập các thanh ghi đa năng",
+      "D. Khối điều khiển để thi hành lệnh một cách tuần tự và tác động lên các mạch chức năng nhằm thi hành lệnh.",
+    ],
+  },
+];
